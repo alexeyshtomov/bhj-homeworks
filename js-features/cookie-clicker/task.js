d1ff37fc6501e710c5eb797b83b6f04b3827d3ef
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-07 22:27:21 +0300

    попытка

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,30 @@
-//плановая проверка VSCODE
+
+const clickerCounter = document.getElementById("clicker__counter");
+const cookieImage = document.getElementById("cookie");
+
+
+let isCookieExpanded = true;
+
+
+let clickCount = 0;
+
+
+cookieImage.addEventListener("click", () => {
+  
+  clickCount++;
+  clickerCounter.textContent = clickCount;
+
+  
+  if (isCookieExpanded) {
+    cookieImage.style.width = "150px";
+    cookieImage.style.height = "150px";
+  } else {
+    cookieImage.style.width = "200px";
+    cookieImage.style.height = "200px";
+  }
+
+  
+  isCookieExpanded = !isCookieExpanded;
+});
+
+//мое первое задание
