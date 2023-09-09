f0bf2e45322a03bfc1ca0a86af7201ff354f41f1
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-09 22:17:55 +0300

    7667

---
+++ client-state/popup/task.js
@@ -0,0 +1,32 @@
+
+function closeModal() {
+    const modal = document.getElementById("subscribe-modal");
+    modal.classList.remove("modal_active");
+
+    
+    document.cookie = "modalClosed=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}
+
+
+function checkModalClosed() {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === "modalClosed" && value === "true") {
+            return true; 
+        }
+    }
+    return false; 
+}
+
+
+window.onload = function() {
+    if (!checkModalClosed()) {
+        const modal = document.getElementById("subscribe-modal");
+        modal.classList.add("modal_active");
+    }
+};
+
+
+const closeBtn = document.querySelector(".modal__close");
+closeBtn.addEventListener("click", closeModal);
