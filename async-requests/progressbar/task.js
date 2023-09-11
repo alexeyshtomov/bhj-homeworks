2dae65210c64a20afac42772da51c219eeafc5e2
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-11 21:44:35 +0300

    976

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,39 +1,46 @@
+// Получаем элементы из DOM
 const form = document.getElementById('form');
 const fileInput = document.getElementById('file');
 const progressBar = document.getElementById('progress');
 
-
+// Добавляем обработчик события отправки формы
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  
+  // Создаем объект FormData для отправки файла
   const formData = new FormData();
   formData.append('file', fileInput.files[0]);
 
- 
+  // Создаем объект XMLHttpRequest
   const xhr = new XMLHttpRequest();
 
-  
+  // Настраиваем обработчик события прогресса загрузки
   xhr.upload.addEventListener('progress', (e) => {
     if (e.lengthComputable) {
-      
+      // Вычисляем прогресс и обновляем значение атрибута value у элемента progress
       const percentComplete = (e.loaded / e.total) * 100;
       progressBar.value = percentComplete;
     }
   });
 
-  
+  // Настраиваем обработчик события завершения загрузки
   xhr.addEventListener('load', () => {
     if (xhr.status === 200) {
-      
+      // Загрузка успешно завершена
       console.log('Файл успешно загружен');
     } else {
-      
+      // В случае ошибки вы можете обработать её здесь
       console.error('Произошла ошибка при загрузке файла');
     }
   });
 
-  
+  // Отправляем AJAX-запрос на сервер
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
   xhr.send(formData);
 });
+
+// Устанавливаем значение прогресс-бара при загрузке страницы
+window.addEventListener('load', () => {
+  progressBar.value = 0; // Устанавливаем начальное значение в 0%
+});
+
