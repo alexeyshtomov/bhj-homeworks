d382376aa79b1ea999d8be994e34750323e2b2ed
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-13 21:08:44 +0300

    ошибки

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,31 +1,28 @@
 const itemsContainer = document.getElementById('items');
 const loader = document.getElementById('loader');
 
-
 fetch('https://students.netoservices.ru/nestjs-backend/slow-get-courses')
   .then((response) => response.json())
   .then((data) => {
-    
     itemsContainer.innerHTML = '';
 
-    
-    for (const currencyCode in data.ответ.Значение) {
-      if (data.ответ.Значение.hasOwnProperty(currencyCode)) {
-        const currency = data.ответ.Значение[currencyCode];
+    for (const currencyCode in data.response.Values) {
+      if (data.response.Values.hasOwnProperty(currencyCode)) {
+        const currency = data.response.Values[currencyCode];
         const item = document.createElement('div');
         item.classList.add('item');
         item.innerHTML = `
           <div class="item__code">${currency.CharCode}</div>
           <div class="item__value">${currency.Value}</div>
-          <div class="item__currency">руб.</div>
+          <div class="item__currency">rub</div>
         `;
         itemsContainer.appendChild(item);
       }
     }
 
-    
     loader.classList.remove('loader_active');
   })
   .catch((error) => {
-    console.error('Произошла ошибка при загрузке данных:', error);
+    console.error('An error occurred while loading data:', error);
   });
+
