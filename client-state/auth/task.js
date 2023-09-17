459a742df0bd1b63f28cca364814a57618dce8e9
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-17 19:00:42 +0300

    ошибки

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const signinForm = document.getElementById("вход_в_форму"); 
+    const signinForm = document.getElementById("вход_в_форму");
     const signinBtn = document.getElementById("signin__btn");
-    const welcomeBlock = document.getElementById("добро_пожаловать"); 
+    const welcomeBlock = document.getElementById("добро_пожаловать");
     const userIdSpan = document.getElementById("user_id");
 
     const savedUserId = localStorage.getItem("user_id");
@@ -13,29 +13,21 @@ document.addEventListener("DOMContentLoaded", function () {
     signinForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        const login = signinForm.querySelector('[name="login"]').value;
-        const password = signinForm.querySelector('[name="password"]').value;
+        const login = signinForm.querySelector('[name="логин"]').value;
+        const password = signinForm.querySelector('[name="пароль"]').value;
 
         const data = {
             login: login,
             password: password,
         };
 
-        fetch("https://students.netoservices.ru/nestjs-backend/auth", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-            .then(function (response) {
-                if (response.status === 200) {
-                    return response.json();
-                } else {
-                    throw new Error("Ошибка сервера: " + response.status);
-                }
-            })
-            .then(function (result) {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const result = JSON.parse(xhr.responseText);
                 if (result.success) {
                     localStorage.setItem("user_id", result.user_id);
                     userIdSpan.textContent = result.user_id;
@@ -43,12 +35,19 @@ document.addEventListener("DOMContentLoaded", function () {
                 } else {
                     alert("Неверный логин/пароль");
                 }
-            })
-            .catch(function (error) {
-                console.error("Произошла ошибка при отправке запроса:", error);
+            } else {
+                console.error("Ошибка сервера: " + xhr.status);
                 alert("Произошла ошибка при отправке запроса. Пожалуйста, попробуйте еще раз.");
-            });
+            }
+        };
+
+        xhr.onerror = function () {
+            console.error("Произошла ошибка при отправке запроса.");
+            alert("Произошла ошибка при отправке запроса. Пожалуйста, попробуйте еще раз.");
+        };
+
+        xhr.send(JSON.stringify(data));
     });
-})
+});
 // проверяем
 
